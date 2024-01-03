7c180ecc9f14d646fc620b05ea2dd6194648654f
jalil <jalil@1001.digital>
2024-01-03 15:10:05 +0100

    Mock up our signature stream

---
+++ components/CheckListItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :style="`color: ${checksEditionColor}`">
+    <Icon type="check" />
+  </div>
+</template>
+
+<script setup>
+const { signature } = defineProps({
+  signature: Object,
+})
+
+const hexNumber = computed(() => BigInt(signature.address))
+const checksEditionColorIndex = computed(() => hexNumber.value % 80n)
+const checksEditionColor = computed(() => EDITION_COLORS[checksEditionColorIndex.value])
+</script>
+
+<style lang="postcss" scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  container-type: inline-size;
+
+  .icon {
+    width: 100cqw;
+    height: 100cqw;
+  }
+}
+</style>
--- components/ChecksStream.vue
+++ components/ChecksStream.vue
@@ -1,5 +1,93 @@
 <template>
-  <div>
-
+  <div class="checks-stream">
+    <CheckListItem
+      v-for="(signature, index) in signatures"
+      :key="index"
+      :signature="signature"
+    />
   </div>
 </template>
+
+<script setup>
+const signatures = [
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+  { address: '0x5efdB6D8c798c2c2Bea5b1961982a5944F92a5C1' },
+  { address: '0xe11Da9560b51f8918295edC5ab9c0a90E9ADa20B' },
+  { address: '0xD1295FcBAf56BF1a6DFF3e1DF7e437f987f6feCa' },
+]
+</script>
+
+<style lang="postcss" scoped>
+.checks-stream {
+  display: grid;
+  grid-template-columns: repeat(8, minmax(0, 1fr));
+  gap: var(--padding);
+  padding: 0 calc(var(--padding-lg) - var(--size-1)) var(--padding);
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
